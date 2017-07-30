1c063b4894d199ac534e3a6c6ba08b2e47d16b38
ekkilm <ekkilm.ts@gmail.com>
2017-07-30 20:39:11 +0300

    Clean up

--- board/gestures.mk
+++ board/gestures.mk
@@ -1,7 +1,2 @@
 # Double tap to wake
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchscreen/double_tap_enable"
-TARGET_GESTURES_NODE := "/proc/touchscreen/gesture_enable"
-TARGET_POWER_GESTURE_FILE := device/xiaomi/libra/modules/power/gestures.c
-TARGET_DRAW_V_NODE := "/proc/touchscreen/flashlight_enable"
-TARGET_DRAW_O_NODE := "/proc/touchscreen/camera_enable"
-TARGET_DRAW_ARROW_LEFT_NODE := "/proc/touchscreen/music_enable"
